0bdc7f6ebcc24925be07700c1012def79cc740cc
Anubhav Prashant Kharkar <anubhavprashantkharkar@Anubhavs-MacBook-Pro.local>
2026-01-06 20:11:51 +0530

    working with fs module

---
+++ nodeprac.js
@@ -0,0 +1,35 @@
+const fs = require('node:fs');
+
+// fs.writeFile("hey.txt","hey hello how are you",function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+
+
+// fs.appendFile("hey.txt","hope you are in a pink of health",function(err){
+//     if(err)console.error(err);
+//     else console.log("done");
+// })
+
+
+// fs.rename("hey.txt","hello.txt",function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+
+// fs.copyFile("hello.txt","./copy/copy.txt",function(err){
+//     if(err)console.error(err);
+//     else console.log("done");
+// })
+
+// fs.unlink("hello.txt",function(err){
+//     if(err)console.error(err);
+//     else console.log("done")
+// });
+
+// fs.rm("./copy",{recursive:true},function(err){
+//     if(err)console.error(err);
+//     else console.log("removed");
+// })
+
+//how to make a folder using fs , how to read a folder , how to read a file
---
+++ script2.js
@@ -0,0 +1,39 @@
+//Node.js basics
+
+
+//Intro to Node.js
+//Installing Node.js & npm
+
+//node & npm
+//working with node and npm
+//npm init
+//node basic usage
+
+//working with modules
+//file system ops
+//understanding http modules
+
+//node JS not a programming lang
+//node js is not a technology as well
+//node js is not a framework as well
+//node js is not a library as well
+
+//actually node js is a js runtime environment
+
+
+//SHORT STORY:
+////js se backend nahi ban sakta
+//bcoz js ke paas wo functionalities hi nahi hai jinse backend banta hai
+//ryan dahl thought ki js se backend banna chahiye pura(servers,databases sab)
+//google chrome ka v8 engine ka code open sourced hai and ryan dahl usme chedkaani kar raha hai
+//hum js ka code likhenge aur wo code wrapper layer of js receive kregi and
+// vo code v8 engine ke cpp modules ke saath ek server create karega
+
+//which implies that nodejs= Chrome's v8engine + JS wrapper
+
+//node js is a javascript runtime environment , kyuki aap javascript se backend nahi create kar sakte
+//aap server NAHI bana sakte js se
+
+//jab tak node chalega tab tak backend ban sakta hai
+
+//kulmilaake , node js ek mahaul deta hai javascript se backend banane keliye
